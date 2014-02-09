e5965588aa990af0de6e20f186f873765cee0caf
Marc Hibbins <marchibbins@gmail.com>
2014-02-09 20:18:05 +0000

    Add basic recipe detail with routes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,17 @@
 'use strict';
 
-angular.module('foodooApp', [
-    'ngResource'
-]);
+angular.module('foodooApp', ['ngResource', 'ngRoute'])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/recipe_list.html',
+                controller: 'RecipeListCtrl'
+            })
+            .when('/recipe/:slug', {
+                templateUrl: 'views/recipe_detail.html',
+                controller: 'RecipeDetailCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+        });
--- app/scripts/controllers/recipes.js
+++ app/scripts/controllers/recipes.js
@@ -3,4 +3,9 @@
 angular.module('foodooApp')
     .controller('RecipeListCtrl', function ($scope, Recipes) {
         $scope.recipeList = Recipes.query();
+    })
+    .controller('RecipeDetailCtrl', function ($scope, $routeParams, Recipes) {
+        $scope.recipe = Recipes.get({
+            slug: $routeParams.slug
+        });
     });
